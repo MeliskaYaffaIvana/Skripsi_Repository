7a47f2ae0f8c2639f3b09ee41041b0835bb44268
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-04 15:16:31 +0700

    coba

--- test/app/Providers/EventServiceProvider.php
+++ test/app/Providers/EventServiceProvider.php
@@ -6,8 +6,6 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use App\Events\StatusJobUpdated;
-use App\Listeners\UpdateTanggalSelesai;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -20,9 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        StatusJobUpdated::class => [
-            UpdateTanggalSelesai::class,
-    ],
     ];
 
     /**
