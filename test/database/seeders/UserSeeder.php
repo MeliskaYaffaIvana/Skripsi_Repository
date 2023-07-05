d60722689337af11fa9a547273bc68ba1cca7b38
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-05 12:06:56 +0700

    ubah seeder

--- test/database/seeders/UserSeeder.php
+++ test/database/seeders/UserSeeder.php
@@ -18,14 +18,6 @@ class UserSeeder extends Seeder
     {
         $users=[
             [
-                'nim' => '1941720020',
-                'nama' => 'Meliska',
-                'judul' => 'Docker',
-                'deskripsi' => 'Repository',
-                'password' => Hash::make('ikaaa123'),
-                'status'=>'mahasiswa'
-            ],
-            [
                 'nim' => '1',
                 'nama' => 'Admin',
                 'judul' => 'Admin',
@@ -34,17 +26,9 @@ class UserSeeder extends Seeder
                 'status'=>'administrator'
             ],
         ];
-        foreach($users as $user){
-        User::create([
-            'nim' => $user['nim'],
-            'nama' => $user['nama'],
-            'judul' => $user['judul'],
-            'deskripsi' => $user['deskripsi'],
-            'password' => $user['password'],
-            'status' => $user['status'],
-            'id' => User::getIdFromNim($user['nim']),
-        ]);
-        \App\Models\User::factory()->count(5)->create(); 
+        foreach ($users as $userData) {
+            $userData['id'] = User::getIdFromNim($userData['nim']);
+            User::create($userData);
         }
     }
 }
