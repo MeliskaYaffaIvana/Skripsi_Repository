05bb9ccee68a37f678d2fbe9c50a37add279c82e
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-28 07:28:54 +0700

    .

--- test/resources/views/wetty.blade.php
+++ test/resources/views/wetty.blade.php
@@ -1,11 +1,41 @@
-<!-- resources/views/wetty.blade.php -->
+@extends('layout')
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Terminal</title>
-</head>
-<body>
-    <iframe src="{{ $url }}" style="width: 100%; height: 100vh; border: none;"></iframe>
-</body>
-</html>
+@section('content')
+<!-- partial -->
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header align-items-center d-flex">
+                        <h4 class="card-title mb-0 flex-grow-1">
+                            <li>Membuat Request Container
+                        </h4>
+                        <div class="flex-shrink-0">
+                        </div>
+                    </div><!-- end card header -->
+
+                    <div class="card-body">
+                        <div id="Container">
+                            <div class="row g-4 mb-3">
+                                <div class="col-sm-auto">
+                                    <iframe src="{{ $url }}" style="width: 80%; height: 100vh; border: none;"></iframe>
+                                </div>
+                            </div>
+
+                            </td>
+                            </tr>
+                        </div>
+                        <!-- end card
+                    </div>
+                    <!-- end col -->
+                    </div>
+                    <!-- end col -->
+                </div>
+                <!-- main-panel ends -->
+            </div>
+            <!-- page-body-wrapper ends -->
+        </div>
+        <!-- container-scroller -->
+
+@endsection
