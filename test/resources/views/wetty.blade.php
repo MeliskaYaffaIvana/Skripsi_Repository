d664feef5f88ee6943f913c14f09ae8b819e3edb
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-28 10:55:07 +0700

    .

--- test/resources/views/wetty.blade.php
+++ test/resources/views/wetty.blade.php
@@ -1,11 +1,51 @@
+@extends('layout')
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Terminal</title>
-</head>
-<body>
-    {{ $url }}
-    <!-- <iframe src="http://10.0.0.21:8181/?command={{ $url }}" style="width: 80%; height: 300px; border: 2px solid black;"></iframe> -->
-</body>
-</html>
+@section('content')
+<style>
+/* Ganti lebar dan tinggi gambar sesuai yang Anda inginkan */
+.gambar {
+    display: block;
+    max-width: 200px;
+    height: auto;
+    margin-top: 10px;
+}
+</style>
+
+<!-- partial -->
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header align-items-center d-flex">
+                        <h4 class="card-title mb-0 flex-grow-1">Akses Kontainer
+                        </h4>
+                        <div class="flex-shrink-0">
+                        </div>
+                    </div><!-- end card header -->
+
+                    <div class="card-body">
+                        <div id="Container">
+                            <div class="row g-4 mb-3">
+                                <div class="col-sm-auto">
+                                    {{ $url }}
+                                     <iframe src="http://10.0.0.21:8181/?command={{ $url }}" style="width: 80%; height: 300px; border: 2px solid black;"></iframe>
+                                </div>
+                            </div>
+
+                            </td>
+                            </tr>
+                        </div>
+                        <!-- end card
+                    </div>
+                    <!-- end col -->
+                    </div>
+                    <!-- end col -->
+                </div>
+                <!-- main-panel ends -->
+            </div>
+            <!-- page-body-wrapper ends -->
+        </div>
+        <!-- container-scroller -->
+
+@endsection
