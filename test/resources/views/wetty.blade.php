a93f7c2a90778517ecf9047eebd1d26946f29959
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-07-28 07:36:20 +0700

    .

--- test/resources/views/wetty.blade.php
+++ test/resources/views/wetty.blade.php
@@ -9,7 +9,7 @@
                 <div class="card">
                     <div class="card-header align-items-center d-flex">
                         <h4 class="card-title mb-0 flex-grow-1">
-                            <li>Membuat Request Container
+                            Membuat Request Container
                         </h4>
                         <div class="flex-shrink-0">
                         </div>
@@ -19,23 +19,17 @@
                         <div id="Container">
                             <div class="row g-4 mb-3">
                                 <div class="col-sm-auto">
-                                    <iframe src="{{ $url }}" style="width: 50%; height: 300px; border: 2px solid black;"></iframe>
+                                    <!-- Gunakan CSS untuk mengatur ukuran card dan iframe -->
+                                    <div class="iframe-container" style="width: 50%; height: 300px; border: 2px solid black;">
+                                        <iframe src="{{ $url }}" style="width: 100%; height: 100%; border: none;"></iframe>
+                                    </div>
                                 </div>
                             </div>
-
-                            </td>
-                            </tr>
                         </div>
-                        <!-- end card
-                    </div>
-                    <!-- end col -->
-                    </div>
-                    <!-- end col -->
-                </div>
-                <!-- main-panel ends -->
-            </div>
-            <!-- page-body-wrapper ends -->
-        </div>
-        <!-- container-scroller -->
-
+                    </div><!-- end card body -->
+                </div><!-- end card -->
+            </div><!-- end col -->
+        </div><!-- end row -->
+    </div><!-- end content wrapper -->
+</div><!-- end main panel -->
 @endsection
